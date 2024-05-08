dc57cea8c6b8b9e89729d43f30db6c51f2c2d71b
Sandil Adhikari <adhikarisandil32@gmail.com>
2024-05-08 14:29:14 +0545

    message

--- index.js
+++ index.js
@@ -13,7 +13,7 @@ app.get("/", function (req, res) {
   res.sendFile(process.cwd() + "/views/index.html")
 })
 
-app.post("/api/fileanalyse", upload.single("uploadedFile"), (req, res) => {
+app.post("/api/fileanalyse", upload.single("upfile"), (req, res) => {
   return res.json({
     name: req.file.originalname,
     type: req.file.mimetype,
